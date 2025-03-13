5082e2430b39af6a056eb89b01382c7aa72e3af7
arman <arman.u.alam@gmail.com>
2025-03-14 00:05:09 +0600

    sample slices

--- lists/lists.go
+++ lists/lists.go
@@ -3,8 +3,24 @@ package main
 import "fmt"
 
 func main() {
+
+	var productNames [4]string = [4]string{"laptop", "mouse", "keyboard", "monitor"}
+
 	// Declare a list of 4 float64 values, known as an array
 	prices := [4]float64{10.0, 20.0, 30.0, 40.0}
 
-	fmt.Println("Prices: ", prices)
+	// access the 1st element of the list
+	fmt.Println("1st price ", prices[0])
+
+	// initialize the 1st element of the list
+	productNames[0] = "tablet"
+	fmt.Println("1st product ", productNames[0])
+
+	// slice: a subset of an array
+	// sampleSlice := productNames[:3] // slice from beginning to 3rd index, excluding 3rd index
+	// sampleSlice := productNames[1:] // slice from beginning to last index
+
+	sampleSlice := productNames[1:3] // slice from 1st to 3rd index, excluding 3rd index
+
+	fmt.Println("sampleSlice ", sampleSlice) //[mouse keyboard]
 }

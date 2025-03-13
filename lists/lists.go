a90f30032eb05cb9c4fdaa931a5de668b9fab603
arman <arman.u.alam@gmail.com>
2025-03-13 23:31:32 +0600

    sample list in go

---
+++ lists/lists.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare a list of 4 float64 values, known as an array
+	prices := [4]float64{10.0, 20.0, 30.0, 40.0}
+
+	fmt.Println("Prices: ", prices)
+}

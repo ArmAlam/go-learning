872223b8acf9cf7d2e69d9a73e940e1fdf8eafe6
arman <arman.u.alam@gmail.com>
2025-03-22 12:04:55 +0600

    feat: insert events into db

--- go-rest-api/models/event.go
+++ go-rest-api/models/event.go
@@ -1,11 +1,14 @@
 package models
 
-import "time"
+import (
+	"example/rest-api/db"
+	"time"
+)
 
 var events = []Event{}
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,9 +16,35 @@ type Event struct {
 	UserId      int
 }
 
-func (e Event) Save() {
-	// TODO: SAVE TO DATABASE
-	events = append(events, e)
+func (e Event) Save() error {
+
+	query := `
+	INSERT INTO events (name, description, location, dateTime, user_id)
+	VALUES (?, ?, ?, ?, ?)
+	`
+
+	// prepare can lead to better performance in certain situation
+	statement, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer statement.Close()
+
+	result, err := statement.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserId)
+
+	if err != nil {
+		return err
+	}
+
+	// get last inserted it
+	id, err := result.LastInsertId()
+
+	e.ID = id
+
+	return err
+
 }
 
 func GetAllEvents() []Event {

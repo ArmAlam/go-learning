5fa16279000c7c1b9e65a673449947ee1869fd87
arman <arman.u.alam@gmail.com>
2025-03-23 17:56:01 +0600

    feat: update event

--- go-rest-api/routes/events.go
+++ go-rest-api/routes/events.go
@@ -64,3 +64,39 @@ func createEvent(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Couldn't get events. Something went wrong"})
+		return
+	}
+
+	_, err = models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Couldn't get event"})
+		return
+	}
+
+	var updatedEvent models.Event
+
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Couldn't update event. Something went wrong"})
+		return
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Couldn't update event. Something went wrong"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated"})
+
+}

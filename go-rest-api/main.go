ff195fcc760e01ee42a904d57516ba4813b0ab97
arman <arman.u.alam@gmail.com>
2025-03-25 16:11:49 +0600

    read port from .env file

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"example/rest-api/db"
 	"example/rest-api/routes"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	// Load environment variables from .env file (optional)
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system environment variables")
+	}
+
+	PORT := os.Getenv("PORT")
 
 	db.InitDB()
 
@@ -15,6 +26,6 @@ func main() {
 
 	routes.RegisterRoutes(server)
 
-	server.Run(":8080")
+	server.Run(":" + PORT)
 
 }

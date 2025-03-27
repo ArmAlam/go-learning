29e84c6adccd5a6085afe7621f7b627302a21592
arman <arman.u.alam@gmail.com>
2025-03-27 16:24:05 +0600

    mutex

---
+++ locks/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	counter int
+	mu      sync.Mutex
+)
+
+func increment() {
+	mu.Lock()
+	defer mu.Unlock()
+	counter++
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			increment()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Final counter value:", counter)
+}
